2767d9cef7aff704b495c44993b45b93017a3f47
Jessamyn Smith <geekchick77@gmail.com>
2015-07-27 23:08:05 -0400

    Integrated updated win code.

--- connect4.py
+++ connect4.py
@@ -3,23 +3,27 @@ import thread #Need this
 import random #Need this
 import time #need this
 
-
 root = Tk() #Need this
+#size = 500?
 root.title("Connect Four") #need this
+
+#since Connect Four is 7x6, making padding 100/50 allows for a buffer of 2 pixels left/right/above/below Connect Four board
 
 # Normally 7 and 6, but could make bigger board
 cols = 7
 rows = 6
 square_size = 50
 padding = 100
+#xSize = ySize
 width = square_size * cols + 2 * padding  # Add padding on each side
 height = square_size * rows + 2 * padding  # Add padding on top and bottom
 
-root.geometry("%sx%s" % (width, height))  # Need this
+root.geometry("%sx%s" % (width, height))  # Why change from original "root.geometry("500x500")"
+# I changed this because the new board doesn't fit well in 500 square
 
 #create connect four board (not the graphics...just the
 #logical board representation)
-board = []
+board = []  #  More rows & cols
 for col in range(cols):
         col_data = []
         for row in range(rows):
@@ -33,7 +37,7 @@ canvas = Canvas(root, width=width, height=height)  # Need this
 
 #Frame rate is how often canvas will be updated
 # each second. For Connect four, 10 should be plenty.
-FRAME_RATE = 10  # this is fine
+FRAME_RATE = 10  # this is fine (SLEEP VS UPDATE)
 
 
 #Function responsible for starting the drawing thread
@@ -49,6 +53,28 @@ def drawThread():
         global canvas, root, rows, cols, padding, square_size
         print 'in draw thread'
         over = False
+
+        def draw_board():
+                #counter variable
+                i=0
+
+                #board variable is what stores the X/O/- values.
+                # It's a 2D list. We iterate over it, looking to see
+                # if there is a value that is X or O. If so, we draw
+                # text to the screen in the appropriate spot (based on
+                # i and j.
+                while i < len(board):
+                        j=0
+                        left = padding + i * square_size + 20
+                        while j < len(board[i]):
+                                top = padding + j * square_size + 20
+                                if board[i][j] == 0:
+                                        lst2.append(canvas.create_text(left, top, text="X", font=("helvetica","36")))
+                                elif board[i][j] == 1:
+                                        lst2.append(canvas.create_text(left, top, text="O", font=("helvetica","36")))
+                                j+=1
+
+                        i+=1
 
         # Draw horizontal lines
         for i in range(rows + 1):
@@ -69,26 +95,7 @@ def drawThread():
                 #have been draw would need to be deleted.
                 lst2 = []
                 
-                #counter variable
-                i=0
-                
-                #board variable is what stores the X/O/- values.
-                # It's a 2D list. We iterate over it, looking to see
-                # if there is a value that is X or O. If so, we draw
-                # text to the screen in the appropriate spot (based on
-                # i and j.
-                while i < len(board):
-                        j=0
-                        left = padding + i * square_size + 10
-                        while j < len(board[i]):
-                                top = padding + j * square_size + 10
-                                if board[i][j] == 0:
-                                        lst2.append(canvas.create_text(left, top, text="X"))
-                                elif board[i][j] == 1:
-                                        lst2.append(canvas.create_text(left, top, text="O"))
-                                j+=1
-                        
-                        i+=1
+                draw_board()
 
                 #like the bind() call, this updates the screen
                 root.update_idletasks()
@@ -103,14 +110,17 @@ def drawThread():
                 #if X wins, put appropriate message on screen
                 if winner == 0:
                         over=True
+                        draw_board()
                         canvas.create_text(width/2,height/2, text="X WINS!",font=("helvetica","18"))
                 #if O wins, put appropriate message on screen
                 elif winner == 1: 
                         over=True
+                        draw_board()
                         canvas.create_text(width/2,height/2, text="O WINS!", font=("helvetica","18"))
                 #if draw, put appropriate message on screen
                 if draw() == True:
                         over=True
+                        draw_board()
                         canvas.create_text(width/2,height/2, text="DRAW!", font=("helvetica","18"))
                         
 
@@ -120,31 +130,139 @@ def drawThread():
 # should return -1 for no win, 0 for x win, 1 for o win
 def win():
         #Does a player have four in a row?
-        if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][0] != -1:
+        if board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[2][0] == board[3][0] and board[0][0] != -1:
                 return board[0][0]
-        if board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][0] != -1:
+        if board[1][0] == board[2][0] and board[2][0] == board[3][0] and board[3][0] == board[4][0] and board[1][0] != -1:
                 return board[1][0]
-        if board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][0] != -1:
+        if board[2][0] == board[3][0] and board[3][0] == board[4][0] and board[4][0] == board[5][0] and board[2][0] != -1:
                 return board[2][0]
-    
-        #Does a player have four in a column?
-        if board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[0][0] != -1:
-                return board[0][0]
-        if board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[0][1] != -1:
+        if board[3][0] == board[4][0] and board[4][0] == board[5][0] and board[5][0] == board[6][0] and board[3][0] != -1:
+                return board[3][0] 
+            
+        if board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[2][1] == board[3][1] and board[0][1] != -1:
                 return board[0][1]
-        if board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[0][2] != -1:
+        if board[1][1] == board[2][1] and board[2][1] == board[3][1] and board[3][1] == board[4][1] and board[1][1] != -1:
+                return board[1][1]
+        if board[2][1] == board[3][1] and board[3][1] == board[4][1] and board[4][1] == board[5][1] and board[2][1] != -1:
+                return board[2][1]
+        if board[3][1] == board[4][1] and board[4][1] == board[5][1] and board[5][1] == board[6][1] and board[3][1] != -1:
+                return board[3][1]
+
+        if board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[2][2] == board[3][2] and board[0][2] != -1:
                 return board[0][2]
+        if board[1][2] == board[2][2] and board[2][2] == board[3][2] and board[3][2] == board[4][2] and board[1][2] != -1:
+                return board[1][2]
+        if board[2][2] == board[3][2] and board[3][2] == board[4][2] and board[4][2] == board[5][2] and board[2][2] != -1:
+                return board[2][2]
+        if board[3][2] == board[4][2] and board[4][2] == board[5][2] and board[5][2] == board[6][2] and board[3][2] != -1:
+                return board[3][2]
+
+        if board[0][3] == board[1][3] and board[1][3] == board[2][3] and board[2][3] == board[3][3] and board[0][3] != -1:
+                return board[0][3]
+        if board[1][3] == board[2][3] and board[2][3] == board[3][3] and board[3][3] == board[4][3] and board[1][3] != -1:
+                return board[1][3]
+        if board[2][3] == board[3][3] and board[3][3] == board[4][3] and board[4][3] == board[5][3] and board[2][3] != -1:
+                return board[2][3]
+        if board[3][3] == board[4][3] and board[4][3] == board[5][3] and board[5][3] == board[6][3] and board[3][3] != -1:
+                return board[3][3]
+
+        if board[0][4] == board[1][4] and board[1][4] == board[2][4] and board[2][4] == board[3][4] and board[0][4] != -1:
+                return board[0][4]
+        if board[1][4] == board[2][4] and board[2][4] == board[3][4] and board[3][4] == board[4][4] and board[1][4] != -1:
+                return board[1][4]
+        if board[2][4] == board[3][4] and board[3][4] == board[4][4] and board[4][4] == board[5][4] and board[2][4] != -1:
+                return board[2][4]
+        if board[3][4] == board[4][4] and board[4][4] == board[5][4] and board[5][4] == board[6][4] and board[3][4] != -1:
+                return board[3][4]
+
+        if board[0][5] == board[1][5] and board[1][5] == board[2][5] and board[2][5] == board[3][5] and board[0][5] != -1:
+                return board[0][5]
+        if board[1][5] == board[2][5] and board[2][5] == board[3][5] and board[3][5] == board[4][5] and board[1][5] != -1:
+                return board[1][5]
+        if board[2][5] == board[3][5] and board[3][5] == board[4][5] and board[4][5] == board[5][5] and board[2][5] != -1:
+                return board[2][5]
+        if board[3][5] == board[4][5] and board[4][5] == board[5][5] and board[5][5] == board[6][5] and board[3][5] != -1:
+                return board[3][5]
+               
+        #Does a player have four in a column?
+        if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][2] == board[0][3] and board[0][0] != -1:
+                return board[0][0]
+        if board[0][1] == board[0][2] and board[0][2] == board[0][3] and board[0][3] == board[0][4] and board[0][1] != -1:
+                return board[0][1]
+        if board[0][2] == board[0][3] and board[0][3] == board[0][4] and board[0][4] == board[0][5] and board[0][2] != -1:
+                return board[0][2]
+            
+        if board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][2] == board[1][3] and board[1][0] != -1:
+                return board[1][0]
+        if board[1][1] == board[1][2] and board[1][2] == board[1][3] and board[1][3] == board[1][4] and board[1][1] != -1:
+                return board[1][1]
+        if board[1][2] == board[1][3] and board[1][3] == board[1][4] and board[1][4] == board[1][5] and board[1][2] != -1:
+                return board[1][2]
+
+        if board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][2] == board[2][3] and board[2][0] != -1:
+                return board[2][0]
+        if board[2][1] == board[2][2] and board[2][2] == board[2][3] and board[2][3] == board[2][4] and board[2][1] != -1:
+                return board[2][1]
+        if board[2][2] == board[2][3] and board[2][3] == board[2][4] and board[2][4] == board[2][5] and board[2][2] != -1:
+                return board[2][2]
+
+        if board[3][0] == board[3][1] and board[3][1] == board[3][2] and board[3][2] == board[3][3] and board[3][0] != -1:
+                return board[3][0]
+        if board[3][1] == board[3][2] and board[3][2] == board[3][3] and board[3][3] == board[3][4] and board[3][1] != -1:
+                return board[3][1]
+        if board[3][2] == board[3][3] and board[3][3] == board[3][4] and board[3][4] == board[3][5] and board[3][2] != -1:
+                return board[3][2]
+
+        if board[4][0] == board[4][1] and board[4][1] == board[4][2] and board[4][2] == board[4][3] and board[4][0] != -1:
+                return board[4][0]
+        if board[4][1] == board[4][2] and board[4][2] == board[4][3] and board[4][3] == board[4][4] and board[4][1] != -1:
+                return board[4][1]
+        if board[4][2] == board[4][3] and board[4][3] == board[4][4] and board[4][4] == board[4][5] and board[4][2] != -1:
+                return board[4][2]
+
+        if board[5][0] == board[5][1] and board[5][1] == board[5][2] and board[5][2] == board[5][3] and board[5][0] != -1:
+                return board[5][0]
+        if board[5][1] == board[5][2] and board[5][2] == board[5][3] and board[5][3] == board[5][4] and board[5][1] != -1:
+                return board[5][1]
+        if board[5][2] == board[5][3] and board[5][3] == board[5][4] and board[5][4] == board[5][5] and board[5][2] != -1:
+                return board[5][2]
+
+        if board[6][0] == board[6][1] and board[6][1] == board[6][2] and board[6][2] == board[6][3] and board[6][0] != -1:
+                return board[6][0]
+        if board[6][1] == board[6][2] and board[6][2] == board[6][3] and board[6][3] == board[6][4] and board[6][1] != -1:
+                return board[6][1]
+        if board[6][2] == board[6][3] and board[6][3] == board[6][4] and board[6][4] == board[6][5] and board[6][2] != -1:
+                return board[6][2]
     
         #Does a player have a diagonal?
-        if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != -1:
-                return board[0][0]
-        if board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[2][0] != -1:
-                return board[2][0]
-
+        if board[0][3] == board[1][2] and board[1][2] == board[2][1] and board[2][1] == board[3][0] and board[0][3] != -1:
+                return board[0][3]
+        if board[0][4] == board[1][3] and board[1][3] == board[2][2] and board[2][2] == board[3][1] and board[0][4] != -1:
+                return board[0][4]
+        if board[1][3] == board[2][2] and board[2][2] == board[3][1] and board[3][1] == board[4][0] and board[1][3] != -1:
+                return board[1][3]
+        if board[0][5] == board[1][4] and board[1][4] == board[2][3] and board[2][3] == board[3][2] and board[0][5] != -1:
+                return board[0][5]
+        if board[1][4] == board[2][3] and board[2][3] == board[3][2] and board[3][2] == board[4][1] and board[1][4] != -1:
+                return board[1][4]
+        if board[2][3] == board[3][2] and board[3][2] == board[4][1] and board[4][1] == board[5][0] and board[2][3] != -1:
+                return board[2][3]
+        if board[1][5] == board[2][4] and board[2][4] == board[3][3] and board[3][3] == board[4][2] and board[1][5] != -1:
+                return board[1][5]
+        if board[2][4] == board[3][3] and board[3][3] == board[4][2] and board[4][2] == board[5][1] and board[2][4] != -1:
+                return board[2][4]
+        if board[3][3] == board[4][2] and board[4][2] == board[5][1] and board[5][1] == board[6][0] and board[3][3] != -1:
+                return board[3][3]
+        if board[2][5] == board[3][4] and board[3][4] == board[4][3] and board[4][3] == board[5][2] and board[2][5] != -1:
+                return board[2][5]
+        if board[3][4] == board[4][3] and board[4][3] == board[5][2] and board[5][2] == board[6][1] and board[3][4] != -1:
+                return board[3][4]
+        if board[3][5] == board[4][4] and board[4][4] == board[5][3] and board[5][3] == board[6][2] and board[3][5] != -1:
+                return board[3][5]
+            
         return -1
 
-#If there is no winner but all the spaces are taken,
-#then there must be a tie.
+#If there is no winner but all the spaces are taken, then there must be a tie.
 def draw():
         for row in board:
                 for cell in row:
